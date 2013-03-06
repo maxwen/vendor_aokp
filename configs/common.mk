f897154c1fe23295ea7517154499dcd394223294
Koushik Dutta <koushd@gmail.com>
2013-03-06 07:09:37 -0800

    Switch to open source superuser
    Change-Id: I0882b4bb296af041ec5766ff29a605554f13b0a7

--- configs/common.mk
+++ configs/common.mk
@@ -1,3 +1,5 @@
+SUPERUSER_EMBEDDED := true
+
 # Common overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/common
 
@@ -10,14 +12,11 @@ PRODUCT_PACKAGES += \
     LatinImeDictionary \
     Microbes \
     ROMControl \
-    SuperSU \
+    Superuser \
+    su \
     SwagPapers \
     Torch \
     UnicornPorn
-
-# Use prebuilt su until fixed when built
-PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/common/xbin/su:system/xbin/su
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
