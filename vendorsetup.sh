7d5f13d3a3bfcf1f967bfddc37c1641af4aa2106
Gergely Szell (sethyx) <szell@hu.ibm.com>
2013-04-15 23:58:39 +0200

    Remove PerformanceControl & a510
    Change-Id: I933fd803e8525e06c06e6ed96906fd9dcaa49318
    Signed-off-by: Gergely Szell (sethyx) <szell@hu.ibm.com>
    
    Conflicts:
    	vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,3 +5,26 @@ add_lunch_combo aokp_toro-userdebug
 # add_lunch_combo aokp_toroplus-userdebug
 add_lunch_combo aokp_grouper-userdebug
 add_lunch_combo aokp_enrc2b-userdebug
+add_lunch_combo aokp_tilapia-userdebug
+add_lunch_combo aokp_manta-userdebug
+add_lunch_combo aokp_d2att-userdebug
+add_lunch_combo aokp_d2tmo-userdebug
+add_lunch_combo aokp_d2usc-userdebug
+add_lunch_combo aokp_d2vzw-userdebug
+add_lunch_combo aokp_evita-userdebug
+add_lunch_combo aokp_hercules-userdebug
+add_lunch_combo aokp_i605-userdebug
+add_lunch_combo aokp_i9100-userdebug
+add_lunch_combo aokp_i9100g-userdebug
+add_lunch_combo aokp_i9300-userdebug
+add_lunch_combo aokp_l900-userdebug
+add_lunch_combo aokp_maserati-userdebug
+add_lunch_combo aokp_p930-userdebug
+add_lunch_combo aokp_solana-userdebug
+add_lunch_combo aokp_spyder-userdebug
+add_lunch_combo aokp_su640-userdebug
+add_lunch_combo aokp_t0lte-userdebug
+add_lunch_combo aokp_targa-userdebug
+add_lunch_combo aokp_umtsspyder-userdebug
+add_lunch_combo aokp_vibrantmtd-userdebug
+add_lunch_combo aokp_vs920-userdebug
