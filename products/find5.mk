1ef3aa545ccfd9228aa8f8f422501d0abc94c22d
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-09-25 15:17:52 +0200

    Partially revert "aokp: World Phone Support"
    This partially revert commit 004201398671ebfdc77d10b9f06afe99f09a6fda
    
    We need to keep the gsm.mk & cdma.mk to specify the
    telefon type.
    
    This fixes the 2G toggle not being visible after boot.
    
    Change-Id: I9ba358a580043c587c16f10c83517f56d9693c13
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- products/find5.mk
+++ products/find5.mk
@@ -4,6 +4,9 @@ $(call inherit-product, device/oppo/find5/full_find5.mk)
 # Inherit AOKP common bits
 $(call inherit-product, vendor/aokp/configs/common.mk)
 
+# Inherit GSM common stuff
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
+
 #Set build fingerprint / ID / Product Name etc.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=find5 BUILD_FINGERPRINT="OPPO/oppo_12069/FIND5:4.1.1/JRO03L/1362469752:user/release-keys" PRIVATE_BUILD_DESC="msm8960-user 4.1.1 JRO03L eng.oppo.20130328.172033 release-keys"
 
